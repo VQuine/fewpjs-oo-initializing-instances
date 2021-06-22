da0ee7473f6bcd42d58a8289dbad0101739751ca
VQuine <01010110.v@gmail.com>
2021-06-22 18:27:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your code here
+// BREAKFAST c l a s s
+
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food
+        this.drink = drink
+    }
+}
+
+// LUNCH c l a s s 
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+    }
+}
+
+// DINNER c l a s s
+
+class Dinner {
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad
+        this.soup = soup
+        this.entree = entree
+        this._dessert = dessert
+    }
+}
